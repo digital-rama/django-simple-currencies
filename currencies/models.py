4d6db68fc55cfad602b30061976b21f44667ae86
digitalrama <rama@digiralrama.co.in>
2021-06-25 11:19:05 +0530

    unicode chnaged to str in models

--- currencies/models.py
+++ currencies/models.py
@@ -13,14 +13,14 @@ class Currency(models.Model):
     symbol = models.CharField(_('symbol'), max_length=4, blank=True)
 
     factor = models.DecimalField(_('factor'), max_digits=10, decimal_places=4, default=1.0,
-        help_text=_('Specifies the difference of the currency to default one.'))
+                                 help_text=_('Specifies the difference of the currency to default one.'))
 
     is_active = models.BooleanField(_('active'), default=True,
-        help_text=_('The currency will be available.'))
+                                    help_text=_('The currency will be available.'))
     is_base = models.BooleanField(_('base'), default=False,
-        help_text=_('Make this the base currency against which rates are calculated.'))
+                                  help_text=_('Make this the base currency against which rates are calculated.'))
     is_default = models.BooleanField(_('default'), default=False,
-        help_text=_('Make this the default user currency.'))
+                                     help_text=_('Make this the default user currency.'))
 
     objects = models.Manager()
     active = CurrencyManager()
@@ -30,16 +30,18 @@ class Currency(models.Model):
         verbose_name = _('currency')
         verbose_name_plural = _('currencies')
 
-    def __unicode__(self):
+    def __str__(self):
         return self.code
 
     def save(self, **kwargs):
         # Make sure the base and default currencies are unique
         if self.is_base is True:
-            self.__class__._default_manager.filter(is_base=True).update(is_base=False)
+            self.__class__._default_manager.filter(
+                is_base=True).update(is_base=False)
 
         if self.is_default is True:
-            self.__class__._default_manager.filter(is_default=True).update(is_default=False)
+            self.__class__._default_manager.filter(
+                is_default=True).update(is_default=False)
 
         # Make sure default / base currency is active
         if self.is_default or self.is_base:
